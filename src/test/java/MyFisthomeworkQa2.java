2519873d25ae4403e801ef3253d72e86ca9cb74a
Alona Solomatova <solomatova.alona@gmail.com>
2021-05-02 18:37:31 +0300

    Changing class

--- src/test/java/MyFisthomeworkQa2.java
+++ src/test/java/MyFisthomeworkQa2.java
@@ -10,16 +10,13 @@ import java.util.List;
 
 public class MyFisthomeworkQa2 {
     private final By ACCEPT_COOKIES_BTN = By.xpath(".//button[@mode = 'primary']");
-    //    private final By LOCATOR_FIRST_ARTICLE = By.xpath(".//*[contains(@href,'https://www.tvnet.lv/7232964/tragedija-bagdade-ugunsgreka-covid-19-pacientu-slimnica-gajusi-boja-82-cilveki')]");
     private final By LOCATOR_FIRST_ARTICLE = By.xpath(".//*[contains(@class,'list-article__media')]");
     private final By LOCATOR_ARTICLE_COMMENTS = By.xpath(".//*[contains(@src,'/v5/img/icons/comment-v2.svg')]");
-    private final By LOCATOR_ARTICLE_TITLE = By.xpath(".//*[contains(@class,'list-article__headline')]");
-    private final By LOCATOR_ALL_ARTICLE_TITLE = By.xpath(".//*[contains(@class,'list-article__headline')]");
+    private final By LOCATOR_ARTICLE_HEADLINE = By.xpath(".//*[contains(@class,'list-article__headline')]");
+    private final By LOCATOR_ALL_ARTICLE_HEADLINE = By.xpath(".//*[contains(@class,'list-article__headline')]");
     private final By LOCATOR_ALL_COUNT_COMMENTS = By.xpath(".//*[contains(@class,'list-article__comment section-font-color')]");
     private final By LOCATOR_LOGO = By.xpath(".//*[@class='flex header-logo flex--align-items-center']");
-    private final By LOCATOR_CHANGE_LANG = By.xpath(".//*[@href='https://rus.tvnet.lv/']");
-//    private final By LOCATOR_CHANGE_LANG = By.xpath(".//a[text()=('RUS')]");
-
+    private final By LOCATOR_CHANGE_LANG = By.xpath(".//a[text()=('RUS')]");
 
     @Test
     public void firstTest() {
@@ -34,7 +31,7 @@ public class MyFisthomeworkQa2 {
         browserWindow.findElement(LOCATOR_FIRST_ARTICLE).click();
         browserWindow.findElement(LOCATOR_ARTICLE_COMMENTS).click();
 
-        browserWindow.findElement(LOCATOR_ARTICLE_TITLE).getText();
+        browserWindow.findElement(LOCATOR_ARTICLE_HEADLINE).getText();
         System.out.println(browserWindow.getTitle());
     }
 
@@ -49,7 +46,7 @@ public class MyFisthomeworkQa2 {
         wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
         browserWindow.findElement(ACCEPT_COOKIES_BTN).click();
 
-        browserWindow.findElements(LOCATOR_ALL_ARTICLE_TITLE).toString();
+        browserWindow.findElements(LOCATOR_ALL_ARTICLE_HEADLINE).toString();
         System.out.println(browserWindow.getTitle());
     }
 
@@ -64,15 +61,15 @@ public class MyFisthomeworkQa2 {
         wait.until(ExpectedConditions.presenceOfElementLocated(ACCEPT_COOKIES_BTN));
         browserWindow.findElement(ACCEPT_COOKIES_BTN).click();
 
-        List <WebElement> allarticle = browserWindow.findElements(LOCATOR_ALL_ARTICLE_TITLE);
-        for (int i=0; i < allarticle.size(); i++){
-            String article = allarticle.get(i).getText();
-            int removeComment = article.indexOf("(");
+        List <WebElement> headline = browserWindow.findElements(LOCATOR_ALL_ARTICLE_HEADLINE);
+        for (int i=0; i < headline.size(); i++){
+            String title = headline.get(i).getText();
+            int removeComment = title.indexOf("(");
             if (removeComment == -1){
-                System.out.println(i+1 + " " + article);
+                System.out.println(i+1 + " " + title);
             }
             else {
-                System.out.println(i+1 + " "+ article.substring(0,removeComment));
+                System.out.println(i+1 + " "+ title.substring(0,removeComment));
             }
 
         }
